5cf4c26d0825d642db959b84c51b22fa22b1be06
cag2050 <cag2050@163.com>
2017-09-07 10:35:35 +0800

    m

--- src/components/Iterator.vue
+++ src/components/Iterator.vue
@@ -1,13 +1,15 @@
 <template lang='pug'>
-<div>
-    <div>为每个li设置不同的样式(数组中包含对象)</div>
-    <ul class='iterator'>
-      <li>li1</li>
-      <li>li2</li>
-      <li>li3</li>
-      <li>li4</li>
-    </ul>
-</div>
+div
+    div 为每个li下的超链接a设置不同的样式(数组中包含对象)
+    ul.iterator
+        li
+            a li1
+        li
+            a li2
+        li
+            a li3
+        li
+            a li4
 </template>
 
 <script>
@@ -26,7 +28,7 @@ items = ({'font-size': 10px, 'left': 10px, 'top': 10px}) ({'font-size': 20px, 'l
 
 ul.iterator
     for item, index in items
-        li:nth-child({index+1})
+        li:nth-child({index+1}) a
             for name, value in item
                 {name} value
 </style>
